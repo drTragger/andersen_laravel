4ab3d9a533c521be0a89b95201c6bdbdcbd395ff
Misha Ponomarenko <mishaponomarenko11082001@gmail.com>
2020-12-18 13:22:41 +0200

    user creation method moved from controller to service

--- app/Http/Controllers/API/UserRegistration.php
+++ app/Http/Controllers/API/UserRegistration.php
@@ -4,18 +4,26 @@ namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
 use App\Models\User;
+use App\services\UserService;
 use Illuminate\Http\Request;
 use App\Http\Requests\RegisterRequest;
 
 class UserRegistration extends Controller
 {
+    protected $userService;
+
+    public function __construct(UserService $userService)
+    {
+        $this->userService = $userService;
+    }
+
     public function register(RegisterRequest $request)
     {
         $data = $request->only(['password', 'email', 'name']);
 
         $data['password'] = bcrypt($request->password);
 
-        $user = User::create($data);
+        $user = $this->userService->createUser($data);
 
         $accessToken = $user->createToken('authToken')->accessToken;
 
