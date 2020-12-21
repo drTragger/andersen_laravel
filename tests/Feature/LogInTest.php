c7a8af7b1dc0735cd20ba20d8991451819dbe62c
Misha Ponomarenko <mishaponomarenko11082001@gmail.com>
2020-12-21 11:26:51 +0200

    Created a test which shows if the user log in succeeds

---
+++ tests/Feature/LogInTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class LogInTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function testLogIn()
+    {
+        $data = [
+            'name' => 'john',
+            'email' => 'email10@example.com',
+            'password' => '1m2i3s4h5a',
+        ];
+
+        $response = $this->json('POST', '/api/login', $data)->assertStatus(202);
+        $response->assertJsonStructure(['access_token'], $response->original);
+    }
+}
