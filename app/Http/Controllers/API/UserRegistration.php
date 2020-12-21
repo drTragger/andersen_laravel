9297f818452b8815263d5dc6f920abf5abbfa526
Misha Ponomarenko <mishaponomarenko11082001@gmail.com>
2020-12-21 11:24:59 +0200

    Created a controller method which gets the user and checks if the password is right

--- app/Http/Controllers/API/UserRegistration.php
+++ app/Http/Controllers/API/UserRegistration.php
@@ -7,6 +7,8 @@ use App\Models\User;
 use App\services\UserService;
 use Illuminate\Http\Request;
 use App\Http\Requests\RegisterRequest;
+use App\Http\Requests\LoginRequest;
+use Illuminate\Support\Facades\Hash;
 
 class UserRegistration extends Controller
 {
@@ -26,5 +28,23 @@ class UserRegistration extends Controller
         $accessToken = $user->createToken('authToken')->accessToken;
 
         return response(['access_token' => $accessToken], 201);
+    }
+
+    public function login(LoginRequest $request)
+    {
+        $data = $request->all();
+
+        $user = $this->userService->checkUser($data);
+
+        if ($user) {
+            if (Hash::check($request->password, $user->password)) {
+                $token = $user->createToken('LogIn')->accessToken;
+                return response(['access_token' => $token], 202);
+            } else {
+                return response(['message' => 'Wrong password'], 422);
+            }
+        } else {
+            return response(['message' => 'User does not exist'], 422);
+        }
     }
 }
