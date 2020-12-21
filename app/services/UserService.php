76713c223110080054faf7075ced1144f5ecf83a
Misha Ponomarenko <mishaponomarenko11082001@gmail.com>
2020-12-21 16:48:12 +0200

    created methods for resetting password in UserService

--- app/services/UserService.php
+++ app/services/UserService.php
@@ -4,6 +4,7 @@
 namespace App\services;
 
 
+use App\Models\ResetPassword;
 use App\Models\User;
 use Illuminate\Support\Facades\Hash;
 
@@ -22,5 +23,35 @@ class UserService
     public function getUserByEmail(string $email)
     {
         return User::where('email', $email)->first();
+    }
+
+    public function generatePasswordResetToken(int $userId)
+    {
+        $data = [
+            'token' => str_random(10),
+            'user_id' => $userId,
+        ];
+        return ResetPassword::create($data);
+    }
+
+    public function getTokenData(string $token)
+    {
+        return ResetPassword::where('token', $token)->first();
+    }
+
+    public function getUserById(int $id)
+    {
+        return User::where('id', $id)->first();
+    }
+
+    public function updatePassword(User $user, string $password)
+    {
+        $user->password = bcrypt($password);
+        $user->update();
+    }
+
+    public function removeToken(string $token)
+    {
+        return ResetPassword::where('token', $token)->delete();
     }
 }
